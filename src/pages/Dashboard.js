a03af46c43e2050e58e747cee26e4fa7c4826d9e
Sean <sgroff04@gmail.com>
2019-01-22 21:53:18 -0600

    basic fetch users collection

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,6 +4,21 @@ import AppMenu from '../components/Menu'
 import { withFirebase } from '../components/Firebase'
 
 class Dashboard extends Component {
+  state = {
+    users: null,
+  }
+
+  async componentDidMount() {
+    const { docs } = await this.props.firebase.db
+      .collection('users')
+      .get()
+      .catch(err => {
+        this.setState(() => ({ users: null }))
+      })
+
+    const users = docs.map(doc => doc.data())
+    this.setState(() => ({ users }))
+  }
   render() {
     const { firebase } = this.props
     return (
